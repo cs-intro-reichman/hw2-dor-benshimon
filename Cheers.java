9f395aadde9f46c09315ecc9f181ae5e0ca26d95
dor-benshimon <dor.benshimon@post.runi.ac.il>
2025-11-06 00:58:33 +0200

    hw22

--- Cheers.java
+++ Cheers.java
@@ -9,5 +9,26 @@ Great work boaz!
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            String word = args[0];
+            int x = Integer.parseInt(args[1]);
+            char[] anletters = {'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R','S', 'X'};
+            word = word.toUpperCase();
+            for(int i = 0 ; i < word.length(); i++){
+                char letter = word.charAt(i);
+                String psic = "a";
+                for (char C : anletters){
+                        if (letter == C ) {
+                psic = "an";
+                break;
+                        }
+                        }
+               System.out.println("Give me " + psic + " " + letter + ":" + letter + "!");
+
+            }
+            System.out.println("What does that spell?");
+            for(int i = 0; i<x; i++){
+            System.out.println("RUNI!!!");
         }
 }
+}
+
