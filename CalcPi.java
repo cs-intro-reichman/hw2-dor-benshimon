150efbf5e25746c5a5b2399ffc3f57b9e288a3fb
dor-benshimon <dor.benshimon@post.runi.ac.il>
2025-11-07 01:29:14 +0200

    w

--- CalcPi.java
+++ CalcPi.java
@@ -18,7 +18,7 @@ public class CalcPi {
 				double newpies = pies*4;
 			
 			System.out.println("pi according to Java: " + Math.PI);
-				System.out.println("pi, approximated: " +newpies);
+			System.out.println("pi, approximated:     " +newpies);
 			
 		
 			 
